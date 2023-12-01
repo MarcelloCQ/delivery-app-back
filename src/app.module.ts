b9502143d30a2717867d8bd03642e5d42b59f90f
Marcello Crescimbeni <giosuemc@gmail.com>
2023-12-01 01:29:02 -0500

    credentials db added

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,7 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "Ymbmcq+1709",
-    database: "delivery-app",
+    url: '',
     synchronize: true,
     logging: true,
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
