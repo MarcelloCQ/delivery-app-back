ea90f343ce0d604b871e18ce6db045e66a013483
Marcello Crescimbeni <giosuemc@gmail.com>
2023-12-03 15:12:45 -0500

    swagger ui added

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth And Register')
+    .setDescription('Auth and register endpoints')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/v1', app, document);
   await app.listen(3000);
 }
 bootstrap();
