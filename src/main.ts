73cabfbacad84bdf337522f72866d32a70d47741
Marcello Crescimbeni <giosuemc@gmail.com>
2023-12-06 15:13:25 -0500

    cors added

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
 
